82d20d7d59f798fe150cb1fafe0be64f59d247be
Soumen Singh <Soumensingh951@gmail.com>
2023-10-02 06:12:41 +0530

    Everithing alright

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './Home';
+import Header from "./components/Header";
+import Footer from './components/Footer'
+import Wtwy from "./Wtwy"
+import HeAssistant from "./HeAssistant"
+import Services from "./Services"
+import News from "./News"
+import Post from "./Post";
+import Getstarted from "./getStarted";
+import DataSet from "./DataSet";
+import OnlineDoctor from "./OnlineDoctor";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/wtwy" element={<Wtwy />} />
+          <Route path="/heassistant" element={<HeAssistant />} />
+          <Route path="/Services" element={<Services />} />
+          <Route path="/news" element={<News />} />
+          <Route path="/post" element={<Post />} />
+          <Route path="/getstarted" element={<Getstarted />} />
+          <Route path="/dataset" element={<DataSet />} />
+          <Route path="/onlinedoctor" element={<OnlineDoctor />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+
     </div>
   );
 }
---
+++ src/OnlineDoctor.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import "./OnlineDoctor.css";
+
+const data = [
+  {
+    name: "Dr Minakshi",
+    department: "Medicine",
+    hospital: "Sat 4pm to 6pm"
+  },
+  {
+    name: "Dr Mrinmoy",
+    department: "General",
+    hospital: "Sat 4pm to 6pm"
+  },
+  {
+    name: "Dr Swarnim",
+    department: "Paediatric",
+    hospital: "Sat 4pm to 6pm"
+  }
+]
+const data2 = [
+  {
+    name: "Dr Pankaj",
+    department: "Physiatrists",
+    hospital: "Sat 4pm to 6pm"
+  },
+  {
+    name: "Dr Pandey",
+    department: "General",
+    hospital: "Sat 4pm to 6pm"
+  },
+
+]
+
+
+const OnlineDoctor = () => {
+
+  return (
+    <div>
+      <header>
+        <h1>Doctor List</h1>
+      </header>
+      <div className="body">
+        {data.map((elm) => (
+
+          <div className='Doctor'>
+            <img src="" alt="" />
+            <div className='details'>
+              <h1>{elm.name}</h1>
+              <h4>{elm.department}</h4>
+              <h3>{elm.hospital}</h3>
+            </div>
+            <div className='book'>
+              <h4>Book</h4>
+            </div>
+          </div>
+        )
+        )}
+      </div>
+      {/* second rpw */}
+      <div className="body">
+        {data2.map((elm) => (
+
+          <div className='Doctor'>
+            <img src="" alt="" />
+            <div className='details'>
+              <h1>{elm.name}</h1>
+              <h4>{elm.department}</h4>
+              <h3>{elm.hospital}</h3>
+            </div>
+            <div className='book'>
+              <h4>Book</h4>
+            </div>
+          </div>
+        )
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default OnlineDoctor
